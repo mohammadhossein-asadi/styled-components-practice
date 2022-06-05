03a86ceb8e0601ae9d2c9f8e7b65ee64d197316a
mohammadhossein <92786003+mohammadhossein-asadi@users.noreply.github.com>
2022-06-05 18:38:23 +0430

    create new file for button is button.styles.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,5 @@
-import styled from "styled-components";
 import "./App.css";
-
-const StyledButton = styled.button`
-  border: 2px solid #4caf50;
-  background-color: #4caf50;
-  color: white;
-  padding: 15px 32px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 16px;
-  cursor: pointer;
-  transition: 0.5s all ease-out;
-`;
+import StyledButton from "./components/Button/Button";
 
 function App() {
   return (
